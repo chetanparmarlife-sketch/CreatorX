7d33e16e41830336816742d4762cef11cf9afd44
Chetan Parmar <chetanparmarlife@gmail.com>
2026-01-14 13:59:42 +0530

    Fixed: Metro Bundling Error

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,11 @@ config.watchFolders = [__dirname];
 config.resolver.blockList = [
   /brand-dashboard\/.*/,
   /backend\/.*/,
+  // Exclude test files from bundling (they're only for Jest)
+  /.*\/__tests__\/.*/,
+  /.*\.test\.(ts|tsx|js|jsx)$/,
+  /.*\.spec\.(ts|tsx|js|jsx)$/,
 ];
 
 module.exports = config;
+
